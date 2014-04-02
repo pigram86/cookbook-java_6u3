5207f8a37629f134a4c238e5657af5ca5c3ea22e
pigram86 <todd@toddpigram.com>
2014-04-02 07:28:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# java_6u3 cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Todd Pigram (<todd@toddpigram.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'java_6u3'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures java_6u3'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
